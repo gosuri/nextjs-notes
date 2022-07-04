13ed1a0b3892b77faf1dc49da988bf3e4a981ff6
Greg Osuri <me@gregosuri.com>
2022-07-04 12:04:17 -0700

    ...

--- nextjs-blog/pages/posts/first-post.js
+++ nextjs-blog/pages/posts/first-post.js
@@ -3,22 +3,25 @@ import Head from 'next/head';
 import Image from 'next/image';
 import Script from 'next/script';
 import profilePic from '../../public/vercel.svg'
+import Layout from "../../components/layout";
 
 
-export default function FirstPost(){
+export default function FirstPost() {
     return (
         <>
-        <Head>
-            <title> First post </title>
-            <Script src="https://connect.facebook.net/en_US/sdk.js" strategy="beforeInteractive" />
-        </Head>
-        <h1>First Post</h1>
-        <h2>
-            <Link href="/">
-                <a>Back</a>
-            </Link>
-        </h2>
-        <Image src={profilePic} alt="MyPic"></Image>
+            <Layout>
+                <Head>
+                    <title> First post </title>
+                    <Script src="https://connect.facebook.net/en_US/sdk.js" strategy="beforeInteractive" />
+                </Head>
+                <h1>First Post</h1>
+                <h2>
+                    <Link href="/">
+                        <a>Back</a>
+                    </Link>
+                </h2>
+                <Image src={profilePic} alt="MyPic"></Image>
+            </Layout>
         </>
     );
 }
