268889bb379a8a135d0b1d6e36128751f0034359
Greg Osuri <me@gregosuri.com>
2022-07-04 10:46:47 -0700

    script wip
    Signed-off-by: Greg Osuri <me@gregosuri.com>

---
+++ nextjs-blog/pages/posts/first-post.js
@@ -0,0 +1,24 @@
+import Link from 'next/link';
+import Head from 'next/head';
+import Image from 'next/image';
+import Script from 'next/script';
+import profilePic from '../../public/vercel.svg'
+
+
+export default function FirstPost(){
+    return (
+        <>
+        <Head>
+            <title> First post </title>
+            <Script src="https://connect.facebook.net/en_US/sdk.js" strategy="beforeInteractive" />
+        </Head>
+        <h1>First Post</h1>
+        <h2>
+            <Link href="/">
+                <a>Back</a>
+            </Link>
+        </h2>
+        <Image src={profilePic} alt="MyPic"></Image>
+        </>
+    );
+}
